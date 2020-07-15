288d64501000d6cf378169c1509385712f76b188
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-07-15 17:43:08 -0300

    Add rate limit to the get requests

--- src/app/services/ffxiv-http-client/ffxiv-http-client.service.ts
+++ src/app/services/ffxiv-http-client/ffxiv-http-client.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable, of} from 'rxjs';
-import {catchError, delay, retry} from 'rxjs/operators';
+import {Observable, Observer, of, Subject} from 'rxjs';
+import {catchError, delay, map, retry, switchMap, tap} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,18 +9,38 @@ import {catchError, delay, retry} from 'rxjs/operators';
 export class FfxivHttpClientService {
 
   baseUrl = 'https://xivapi.com/';
+  timeBetweenRequests = 75;
+  currentRequests = -1;
+  lastRequest = 0;
 
   constructor(private httpClient: HttpClient) {
   }
 
   get(url: string): Observable<any> {
+    return this.executeFunctionWithDelay(() => this.makeGetRequest(url));
+  }
+
+  private makeGetRequest(url: string): Observable<any> {
     return this.httpClient.get(this.baseUrl + url, {
       headers: {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Headers': '*', 'Content-type': 'application/json'}
-    }).pipe(catchError(err => {
-      if (err.status === 429) {
-        return this.get(url).pipe(delay(1000));
+    }).pipe(
+      catchError(err => {
+        if (err.status === 429) {
+          return this.get(url);
+        }
+        return of(undefined);
+      }));
+  }
+
+  private executeFunctionWithDelay(callback: () => Observable<any>): Observable<any> {
+    this.currentRequests++;
+    const now = new Date().getTime();
+    const timeout = this.currentRequests === 0 ? 0 : now - this.lastRequest + this.currentRequests * this.timeBetweenRequests;
+    this.lastRequest = now;
+    return of(undefined).pipe(delay(timeout), switchMap(value => {
+        this.currentRequests--;
+        return callback();
       }
-      of(undefined);
-    }));
+    ));
   }
 }
