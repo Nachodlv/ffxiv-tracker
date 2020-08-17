ed2419ac6e8d02d995718cbcad1ad7fa56792fbf
Ignacio de la Vega <ignacio.delavega@ing.austral.edu.ar>
2020-08-17 19:30:44 -0300

    Add purchase minion pack

--- src/app/models/packs/minion-packs.ts
+++ src/app/models/packs/minion-packs.ts
@@ -209,6 +209,107 @@ const gatheringMinions = new ItemPack('Gathering', ItemType.Minion, [
   '24', // Tiny Tortoise
 ]);
 
+const purchaseMinions = new ItemPack('Purchase', ItemType.Minion, [
+  '380', // Lalinator 5.H0
+  '387', // Sand Fox
+  '363', // Cerberpup
+  '379', // Unlucky Rabbit
+  '368', // Little Leafman
+  '370', // The Behatted Serpent Of Ronka
+  '369', // The Behelmeted Serpent Of Ronka
+  '358', // Cute Justice
+  '360', // Dress-up Estinien
+  '357', // Miniature White Knight
+  '354', // Wind-up Pixie
+  '346', // Tinker's Bell
+  '351', // Ironfrog Ambler
+  '326', // Wind-up Nu Mou
+  '342', // Giant Beaver
+  '340', // Bitty Duckbill
+  '350', // Butterfly Effect
+  '345', // Wind-up Omega-F
+  '344', // Wind-up Omega-M
+  '338', // Micro Gigantender
+  '328', // Zephyrous Zabuton
+  '322', // Wind-up Qalyana
+  '323', // Wind-up Redback
+  '324', // Minitek Conveyor
+  '292', // Frilled Dragon
+  '291', // Clockwork Lantern
+  '298', // Wind-up Zhloe
+  '302', // Attendee #777
+  '277', // Wind-up Ananta
+  '288', // Komainu
+  '264', // Wind-up Hien
+  '266', // Wind-up Kojin
+  '274', // Wind-up Ixion
+  '260', // Wind-up Khloe
+  '256', // Wind-up Meateater
+  '243', // Tora-jiro
+  '242', // Fox Kit
+  '194', // Pegasus Colt
+  '190', // Baby Brachiosaur
+  '236', // Wind-up Moon
+  '228', // Wind-up Estinien
+  '235', // Wind-up Ohl Deeh
+  '218', // Dress-up Alisaie
+  '217', // Dress-up Thancred
+  '198', // Hecteye
+  '196', // Wind-up Sasquatch
+  '187', // Piggy
+  '191', // Wind-up Cheerleader
+  '184', // Wind-up Dragonet
+  '174', // Wind-up Nero Tol Scaeva
+  '156', // Wind-up Gnath
+  '175', // Wind-up Vath
+  '172', // Wind-up Zundu Warrior
+  '135', // Wind-up Gundu Warrior
+  '162', // Hunting Hawk
+  '157', // Paissa Brat
+  '167', // Wind-up Firion
+  '144', // Griffin Hatchling
+  '148', // Behemoth Heir
+  '117', // Water Imp
+  '20', // Black Coeurl
+  '84', // Wind-up Nanamo
+  '106', // Heavy Hatchling
+  '127', // Wind-up Sea Devil
+  '126', // Wind-up Kobolder
+  '59', // Wind-up Ixal
+  '125', // Wind-up Dezul Qualan
+  '124', // Wind-up Founder
+  '123', // Wind-up Violet
+  '85', // Wind-up Gilgamesh
+  '49', // Wind-up Goblin
+  '93', // Treasure Box
+  '83', // Zu Hatchling
+  '82', // Wind-up Succubus
+  '77', // Wind-up Warrior Of Light
+  '65', // Wind-up Sun
+  '76', // Wind-up Odin
+  '61', // Wind-up Sahagin
+  '60', // Wind-up Kobold
+  '67', // Minion Of Light
+  '71', // Wind-up Leader
+  '58', // Wind-up Amalj'aa
+  '50', // Wind-up Sylph
+  '51', // Wind-up Cursor
+  '2', // Mammet #001
+  '37', // Fledgling Dodo
+  '36', // Beady Eye
+  '28', // Dust Bunny
+  '26', // Baby Bat
+  '25', // Baby Raptor
+  '17', // Wide-eyed Fawn
+  '13', // Tiny Rat
+  '1', // Cherry Bomb
+  '54', // Black Chocobo Chick
+  '11', // Flame Hatchling
+  '10', // Serpent Hatchling
+  '9', // Storm Hatchling
+  '3', // Wayward Hatchling
+]);
+
 export const MinionPacks: ItemPack[] = [
   allMinions,
   trialMinions,
@@ -219,5 +320,6 @@ export const MinionPacks: ItemPack[] = [
   dungeonMinions,
   deepDungeonMinions,
   craftingMinions,
-  gatheringMinions
+  gatheringMinions,
+  purchaseMinions
 ];
